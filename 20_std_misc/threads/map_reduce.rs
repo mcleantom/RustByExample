0344a3dc81222973b8b0a19bd2ffdb1a2d0fab4d
mcleantom <mcleantom97@gmail.com>
2023-11-19 21:56:46 +0000

    wip

--- 20_std_misc/threads/map_reduce.rs
+++ 20_std_misc/threads/map_reduce.rs
@@ -1,5 +1,7 @@
 use std::thread;
 
+static NTHREADS: usize = 2;
+
 fn main() {
     // The data to process
     let data = "86967897737416471853297327050364959
@@ -12,30 +14,41 @@ fn main() {
                 16278424637452589860345374828574668";
 
     // Make a vector to hold the child-threads which we will spawn.
-    let mut children = vec![];
+    let mut children: Vec<_> = vec![];
 
-    let chunked_data = data.split_whitespace();
-    
-    // Iterate over the data segments.
-    for (i, data_segment) in chunked_data.enumerate() {
-        println!("data segment {} is \"{}\"", i, data_segment);
+    for (thread_id, chunk) in data.split_whitespace().collect::<Vec<&str>>().chunks(NTHREADS).enumerate() {
+        println!("Thread {}", thread_id);
 
-        // Process each data segment in a separate thread
         children.push(thread::spawn(move || -> u32 {
-            // Calculate the intermediate sum of this segment
-            let result = data_segment
+            let result = chunk
+                .iter()
                 .chars()
                 .map(|c| c.to_digit(10).expect("should be a digit"))
                 .sum();
-
-            println!("processed segment {}, result={}", i, result);
-
             result
         }));
     }
 
-    // Collect and combine the intermediate results into a final result
-    let final_result = children.into_iter().map(|c| c.join().unwrap()).sum::<u32>();
+    // // Iterate over the data segments.
+    // for (i, data_segment) in chunked_data.enumerate() {
+    //     println!("data segment {} is \"{}\"", i, data_segment);
 
-    println!("Final sum result: {}", final_result);
+    //     // Process each data segment in a separate thread
+    //     children.push(thread::spawn(move || -> u32 {
+    //         // Calculate the intermediate sum of this segment
+    //         let result = data_segment
+    //             .chars()
+    //             .map(|c| c.to_digit(10).expect("should be a digit"))
+    //             .sum();
+
+    //         println!("processed segment {}, result={}", i, result);
+
+    //         result
+    //     }));
+    // }
+
+    // // Collect and combine the intermediate results into a final result
+    // let final_result = children.into_iter().map(|c| c.join().unwrap()).sum::<u32>();
+
+    // println!("Final sum result: {}", final_result);
 }
